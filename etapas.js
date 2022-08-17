29e2855cf23b002a0d2b6858b8685d9556200148
thiagowgarcia <thiagowgarcia@gmail.com>
2022-08-17 10:17:41 -0300

    criação da lista de candidatos

---
+++ etapas.js
@@ -0,0 +1,53 @@
+let etapas = [
+  {
+    titulo: 'vereador',
+    numeros: 5,
+    candidatos: [
+      {
+        numero: '10111',
+        nome: 'vereador 01',
+        partido: 'partido 01',
+        fotos: [
+          { url: 'image01.jpeg', legenda: 'vereador' }
+        ]
+      },
+      {
+        numero: '20222',
+        nome: 'vereador 02',
+        partido: 'partido 02',
+        fotos: [
+          { url: 'image02.jpeg', legenda: 'vereador' }
+        ]
+      },
+    ]
+  },
+
+  {
+    titulo: 'Prefeito',
+    numeros: 2,
+    candidatos: [
+      {
+        numero: '10',
+        nome: 'prefeito 01',
+        partido: 'partido 01',
+        vice: 'vice prefeito 01',
+        fotos: [
+          { url: 'image03.jpeg', legenda: 'Prefeito' },
+          { url: 'image04.jpeg', legenda: 'Vice-Prefeito', small: true }
+        ]
+      },
+      {
+        numero: '20',
+        nome: 'candidato 02',
+        partido: 'partido 02',
+        vice: 'vice prefeito 02',
+        fotos: [
+          { url: 'image05.jpeg', legenda: 'vereador' },
+          { url: 'image06.jpeg', legenda: 'Vice-Prefeito', small: true }
+        ]
+      },
+    ]
+  }
+]
+
+
